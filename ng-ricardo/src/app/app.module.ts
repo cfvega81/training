1861bea957965e35b1d3a43c0977688b93730662
Ricardo3142 <davidricardoarias@hotmail.com>
2024-03-15 13:37:03 -0600

    ng-Ricardo versión angular 17
    Co-authored-by: Cesar Fernando Vega Vazquez <cesar.vega@neoris.com>

--- ng-ricardo/src/app/app.module.ts
+++ ng-ricardo/src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,15 +13,29 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SkeletonComponent,
+    FooterComponent,
+    NavigationComponent,
   ],
   imports: [
-
     BrowserModule,
+    BrowserAnimationsModule,
+
+    //Material 
+    MatButtonModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatIconModule,
+
     //core
     CoreModule,
     SharedModule,
