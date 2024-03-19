046247302d52422d6dab0c694e4a547cdc18da8a
Ricardo3142 <davidricardoarias@hotmail.com>
2024-03-18 23:00:21 -0600

    ng-Ricardo-v16 Menu Navbar

--- ng-ricardo-v16/src/app/app.module.ts
+++ ng-ricardo-v16/src/app/app.module.ts
@@ -6,17 +6,44 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkeletonComponent,
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    SharedModule,
+    BrowserAnimationsModule,
+
+    //Material
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+
     //Core
     CoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [
     {
